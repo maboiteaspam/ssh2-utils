2158564d40d22b6211882afb8aab36f040ff52ba
clement <maboiteaspam@wanadoo.fr>
2015-04-18 23:21:50 +0800

    improve machine wake up during test

--- test/index.js
+++ test/index.js
@@ -32,10 +32,14 @@ if( process.env['TRAVIS'] ){
 
 
 if( !process.env['TRAVIS'] ){
+
+  var hasBooted = true;
+
   before(function(done){
     var vagrant = require('child_process').spawn('vagrant',['up'])
     vagrant.stdout.on('data', function (data) {
       console.log('stdout: ' + data);
+      if(hasBooted && data.toString().match(/already running/) ) hasBooted = false;
     });
     vagrant.stderr.on('data', function (data) {
       console.log('stderr: ' + data);
@@ -46,20 +50,26 @@ if( !process.env['TRAVIS'] ){
     });
     this.timeout(50000);
   });
+
   after(function(done){
-    var vagrant = require('child_process').spawn('vagrant',['halt'])
-    vagrant.stdout.on('data', function (data) {
-      console.log('stdout: ' + data);
-    });
-    vagrant.stderr.on('data', function (data) {
-      console.log('stderr: ' + data);
-    });
-    vagrant.on('close', function (code) {
-      console.log('child process exited with code ' + code);
+    if(hasBooted){
+      var vagrant = require('child_process').spawn('vagrant',['halt'])
+      vagrant.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+      });
+      vagrant.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+      });
+      vagrant.on('close', function (code) {
+        console.log('child process exited with code ' + code);
+        done();
+      });
+      this.timeout(50000);
+    } else {
       done();
-    });
-    this.timeout(50000);
+    }
   });
+
 }
 
 
